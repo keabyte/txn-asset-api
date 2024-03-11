93a19d67e388f17b7c66c4370004dc83befbfc7e
Taylar <keast.taylar@gmail.com>
2024-03-11 16:32:37 +1100

    swagger page

--- src/main/kotlin/com/keabyte/transaction_engine/asset_api/TxnAssetApiApplication.kt
+++ src/main/kotlin/com/keabyte/transaction_engine/asset_api/TxnAssetApiApplication.kt
@@ -1,11 +1,20 @@
 package com.keabyte.transaction_engine.asset_api
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+    info = Info(
+        title = "txn-asset-api",
+        version = "0.0.1",
+        description = "API for managing assets"
+    )
+)
 @SpringBootApplication
 class TxnAssetApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<TxnAssetApiApplication>(*args)
+    runApplication<TxnAssetApiApplication>(*args)
 }
